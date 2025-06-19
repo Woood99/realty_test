102e954585ac284589971e7ebf452d0c91e4d454
Woood99 <85289176+Woood99@users.noreply.github.com>
2025-06-19 03:33:06 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 const manifest = {
-   theme_color: '#8936FF',
-   background_color: '#2EC6FE',
+   theme_color: '#fff',
+   background_color: '#fff',
    icons: [
       { purpose: 'maskable', sizes: '512x512', src: 'icon512_maskable.png', type: 'image/png' },
       { purpose: 'any', sizes: '512x512', src: 'icon512_rounded.png', type: 'image/png' },
